387393c304c5e0e3b4387103599a80d126735db0
Jeremy Ahn <jeremy.ahn@dubitlimited.com>
2022-10-20 03:44:55 +0900

    pass tuple

--- train.py
+++ train.py
@@ -256,10 +256,10 @@ if __name__ == '__main__':
                 optimizer.zero_grad()
 
                 if args.fcos:
-                    classification_loss, regression_loss, centerness_loss = retinanet(audio, target)    # retinanet = model.resnet50(**dict_args)
+                    classification_loss, regression_loss, centerness_loss = retinanet((audio, target))  # retinanet = model.resnet50(**dict_args)
                                                                                                         # this calls the forward function of resnet50
                 else:
-                    classification_loss, regression_loss = retinanet(audio, target)
+                    classification_loss, regression_loss = retinanet((audio, target))
                     centerness_loss = torch.zeros(1)
     
                 classification_loss = classification_loss.mean()
